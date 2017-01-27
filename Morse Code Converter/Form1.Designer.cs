0d1cd13c70edbb1359a13d977eb2bb8019f630e5
Hrishi1999 <hrishipatel_5@outlook.com>
2017-01-27 21:31:54 +0530

    First Commit!

---
+++ Morse Code Converter/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Morse_Code_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 61);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(8, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output :";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 130);
+            this.label1.Multiline = true;
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 73);
+            this.label1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Morse Code Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox label1;
+    }
+}
+
---
+++ Morse Code Converter/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_Code_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = "";
+            Dictionary<char, String> mcode = new Dictionary<char, String>()
+            {
+                {'a' , ".-"},
+                { 'b' , "-..."},
+                { 'c' , "-.-."},
+                {'d' , "-.."},
+                { 'e' , "."},
+                { 'f' , "..-."},
+                {'g' , "--."},
+                { 'h' , "...."},
+                { 'i' , ".."},
+                {'j' , ".---"},
+                { 'k' , "-.-"},
+                { 'l' , ".-.."},
+                {'m' , "--"},
+                { 'n' , "-."},
+                { 'o' , "---"},
+                {'p' , ".--."},
+                { 'q' , "--.-"},
+                { 'r' , ".-."},
+                {'s' , ".-."},
+                { 't' , "-"},
+                { 'u' , "..-"},
+                {'v' , "...-"},
+                { 'w' , ".--"},
+                { 'x' , "-..-"},
+                {'y' , "-.--"},
+                { 'z' , "--.."},
+                {'0' , "-----"},
+                { '1' , ".----"},
+                { '2' , "..----"},
+                { '3' , "...--"},
+                {'4' , "....-"},
+                { '5' , "....."},
+                { '6' , "-...."},
+                { '7' , "--..."},
+                {'8' , "---.."},
+                { '9' , "----."},
+            };
+            string userText = textBox1.Text;
+            userText = userText.ToLower();
+            for (int index = 0; index < userText.Length; index++)
+            {
+                char x = userText[index];
+                if (mcode.ContainsKey(x))
+                {
+                    label1.Text += (mcode[x]);
+                }
+            }
+        }
+    }
+}
